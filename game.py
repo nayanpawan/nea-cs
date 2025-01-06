1cbc8d451eac6f6a107b167fbb58781f62775c28
Nayan Pandey <pandeynayan2007@gmail.com>
2025-01-06 10:54:50 +0000

    Button is working but page doesnt change

--- game.py
+++ game.py
@@ -14,22 +14,17 @@ def main_menu():
     BG = pygame.transform.scale(BG, (WIDTH, HEIGHT))
 
     ##title##
-    title_surface = pygame.Surface((1080, 100), pygame.SRCALPHA)
-    title_surface.fill((128, 128, 128, 128))
-
-    font = pygame.font.SysFont("arial", 60)  
-    title_text = font.render("Main Menu", True, (255, 255, 255))
-    title_text_rect = title_text.get_rect(center=(WIDTH//2,50))
+    title=pygame.image.load('title.png').convert_alpha()
+    title_rect=title.get_rect()
+    title_rect.midtop = (WIDTH // 2, 10) 
 
     ##buttons##
 
     while True:
         SCREEN.blit(BG,(0,0))
+        SCREEN.blit(title, title_rect.topleft)
 
-        SCREEN.blit(title_surface, (0, 0))
-        SCREEN.blit(title_text, title_text_rect)
-
-        sign_in_button=Button('sign_in_button.png',(540,200),'hover_sign_in_button.png')
+        sign_in_button=Button('signIn.png',(540,200),'hover_signIn.png')
         mouse_pos=pygame.mouse.get_pos()
         sign_in_button.change_image(mouse_pos)
         sign_in_button.update(SCREEN)

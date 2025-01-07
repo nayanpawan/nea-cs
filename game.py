f2bcf19da51f7d72a3b25a39420a495173bf99ff
Nayan Pandey <pandeynayan2007@gmail.com>
2025-01-07 09:56:51 +0000

    Register and sign in work now need to work on database and play menue

--- game.py
+++ game.py
@@ -10,11 +10,11 @@ SCREEN=pygame.display.set_mode((WIDTH, HEIGHT))
 
 def main_menu():
     pygame.display.set_caption('Main menu')
-    BG=pygame.image.load("one-piece-bg1.jpg").convert_alpha()
+    BG=pygame.image.load("main-menu-bg.png").convert_alpha()
     BG = pygame.transform.scale(BG, (WIDTH, HEIGHT))
 
     ##title##
-    title=pygame.image.load('title.png').convert_alpha()
+    title=pygame.image.load('main-title.png').convert_alpha()
     title_rect=title.get_rect()
     title_rect.midtop = (WIDTH // 2, 10) 
 
@@ -23,16 +23,37 @@ def main_menu():
     while True:
         SCREEN.blit(BG,(0,0))
         SCREEN.blit(title, title_rect.topleft)
-
-        sign_in_button=Button('signIn.png',(540,200),'hover_signIn.png')
         mouse_pos=pygame.mouse.get_pos()
-        sign_in_button.change_image(mouse_pos)
-        sign_in_button.update(SCREEN)
+        
+        sign_in_button=Button('signIn.png',(540,350),'hover_signIn.png')
+        register_button=Button('register.png',(540,450),'register-hover.png')
+        
+        for button in [sign_in_button, register_button]:
+            button.change_image(mouse_pos)
+            button.update(SCREEN)
 
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            if event.type==pygame.MOUSEBUTTONDOWN:
+                if sign_in_button.check_input(mouse_pos):
+                    sign_in_menu()
+                elif register_button.check_input(mouse_pos):
+                    pass
         pygame.display.update()     
+
+def sign_in_menu():
+    pygame.display.set_caption('Sign In')
+    SCREEN.fill((0,0,0))
+    BG=pygame.image.load("sign-in-bg.png").convert_alpha()
+    BG = pygame.transform.scale(BG, (WIDTH, HEIGHT))
+
+    ##title##
+    title=pygame.image.load('sign-in-title.png').convert_alpha()
+    title_rect=title.get_rect()
+    title_rect.midtop = (WIDTH // 2, 10) 
+
+
 
 main_menu()        
